21e2b7ebe39d4713cc7ede7a18f2d245547a7da1
mkarroqe@gmail.com <mkarroqe@gmail.com>
2020-02-16 16:02:17 -0500

    the baby bird ascends
    but mama is lost. while i figure out how to add multiple objects to the scene, this is a good place to go back to if i mess my code up too much

---
+++ Simple-Birdie/SDLProject/main.cpp
@@ -0,0 +1,161 @@
+// Simple Birdie: A Simple 2D Scene
+// by: Mary Karroqe
+// Code adapted from CS3113 Class Template + Triangle Example
+
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+SDL_Window* displayWindow;
+bool gameIsRunning = true;
+
+ShaderProgram program;
+glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
+
+float player_x = 0;
+float player_y = 0;
+float player_rotate = 0;
+
+GLuint birdieTextureID;
+GLuint babyTextureID;
+
+GLuint LoadTexture(const char* filePath) {
+    int w, h, n;
+    unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+    
+    if (image == NULL) {
+        std::cout << "Unable to load image. Make sure the path is correct\n";
+        assert(false);
+    }
+    
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    
+    stbi_image_free(image);
+    
+    return textureID;
+}
+
+void Initialize() {
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("A Simple Birdie and her Babies", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
+    
+#ifdef _WINDOWS
+    glewInit();
+#endif
+    
+    glViewport(0, 0, 640, 480);
+    
+    program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+    
+    viewMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::mat4(1.0f);
+    projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+    
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+//    program.SetColor(1.0f, 0.0f, 0.0f, 1.0f);
+    
+    glUseProgram(program.programID);
+    
+    // light cyan
+    glClearColor(0.878f, 1.0f, 1.0f, 1.0f);
+    
+    // Blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    birdieTextureID = LoadTexture("birdie.png");
+    babyTextureID = LoadTexture("baby_birdie.png");
+}
+
+void ProcessInput() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+            gameIsRunning = false;
+        }
+    }
+}
+
+float lastTicks = 0.0f;
+
+void Update() {
+    float ticks = (float)SDL_GetTicks() / 1000.0f;
+    float deltaTime = ticks - lastTicks;
+    lastTicks = ticks;
+    
+    player_x += 1.0f * deltaTime;
+    player_y += 1.0f * deltaTime;
+    player_rotate += -90.0f * deltaTime;
+    
+    modelMatrix = glm::mat4(1.0f);
+//    modelMatrix = glm::scale(modelMatrix, glm::vec3(3.0f, 3.0f, 1.0f)); // mama bird needs to be bigger
+    
+    modelMatrix = glm::translate(modelMatrix, glm::vec3(0.0f, player_y, 0.0f));
+//    modelMatrix = glm::rotate(modelMatrix, glm::radians(player_rotate), glm::vec3(0.0f, 0.0f, 1.0f));
+    
+//    modelMatrix = glm::mat4(1.0f);
+//    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.09f, 1.0f, 1.0f));
+//    modelMatrix = glm::rotate(modelMatrix, glm::radians(25.0f), glm::vec3(0.0f, 0.0f, 0.01f));
+
+}
+
+void Render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // mama birdie
+    program.SetModelMatrix(modelMatrix);
+
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    
+    glBindTexture(GL_TEXTURE_2D, birdieTextureID);
+    glBindTexture(GL_TEXTURE_2D, babyTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    glDisableVertexAttribArray(program.positionAttribute);
+    glDisableVertexAttribArray(program.texCoordAttribute);
+    
+    SDL_GL_SwapWindow(displayWindow);
+}
+
+void Shutdown() {
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    Initialize();
+    
+    while (gameIsRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
+    
+    Shutdown();
+    return 0;
+}
